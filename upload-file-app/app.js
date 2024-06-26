243420b6bc7fa7d91278c84cb627a0236a8b21dc
Dahu-0712 <419460476@qq.com>
2024-06-26 23:41:10 +0800

    feat：【upload-file-app】文件切片上传（最后合并文件还有问题）

--- upload-file-app/app.js
+++ upload-file-app/app.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
 const path = require('path')
+const fs = require('fs')
+const express = require('express')
 const multer = require('multer')
+const crypto = require('crypto') // 引入crypto模块用于生成唯一标识
+
+const app = express()
+
+app.get('/beforeUpload/', (req, res) => {
+  // 生成一个唯一标识，这里使用时间和随机数的组合
+  const uniqueSuffix = `${Date.now()}-${crypto.randomBytes(6).toString('hex')}`
+  res.status(200).json({
+    status: 'success',
+    data: { id: uniqueSuffix },
+  })
+})
 
 const multerStorage = multer.diskStorage({
   // 文件存储的目录
@@ -10,17 +22,43 @@ const multerStorage = multer.diskStorage({
   },
   // 文件存储的名称
   filename: (req, file, cb) => {
-    console.log(file)
-    const fileName = Buffer.from(file.originalname, 'latin1').toString('utf-8')
+    const fileName = `${Buffer.from(file.originalname, 'latin1').toString(
+      'utf-8'
+    )}`
     cb(null, fileName)
   },
 })
-
 const upload = multer({ storage: multerStorage })
-
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload/:id', upload.single('file'), (req, res) => {
   res.status(200).json({
     status: 'success',
+    data: 'ok',
+  })
+})
+
+app.get('/mergeFile/:id', (req, res) => {
+  const fileName = req.params.id
+  const mergedFilePath = `${fileName}.mp4`
+  // 使用追加模式打开或创建文件
+  fs.writeFileSync(path.join(__dirname, '/uploads', mergedFilePath), '', {
+    flag: 'w',
+  }) // 初始化合并文件，清空或创建
+  fs.readdirSync(path.join(__dirname, '/uploads'))
+    .map((chunkPath) => {
+      if (chunkPath.startsWith(fileName)) {
+        return chunkPath
+      }
+    })
+    .sort((a, b) => {
+      return a - b
+    })
+    .forEach((chunkPath) => {
+      console.log(chunkPath)
+      let data = fs.readFileSync(path.join(__dirname, '/uploads', chunkPath))
+      fs.appendFileSync(path.join(__dirname, '/uploads', mergedFilePath), data)
+    })
+  res.status(200).json({
+    status: 'ok',
     data: 'ok',
   })
 })
